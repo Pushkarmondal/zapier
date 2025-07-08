b5608d3c22d708221a3e79f5e25588b94953e183
Pushkar Mondal <nishitm060@gmail.com>
2025-07-08 20:02:48 +0530

    changes Schema models and added transactions

--- backend/index.ts
+++ backend/index.ts
@@ -3,9 +3,31 @@ import { PrismaClient } from "@prisma/client";
 
 const app = express();
 const prisma = new PrismaClient();
+app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.post("/hooks/catch/:userId/:zapId", async (req, res) => {
+  try {
+    const { userId, zapId } = req.params;
+    const metadata = req.body
+    const transaction = await prisma.$transaction(async (tx) => {
+      const zapruns = await prisma.zapRun.create({
+        data: {
+          zapId,
+          metadata: metadata
+        },
+      });
+      const zaprunoutbox = await prisma.zapRunOutBox.create({
+        data: {
+          zapRunId: zapruns.id,
+        },
+      });
+      return { zapruns, zaprunoutbox };
+    });
+    res.json(transaction);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 });
 
 app.listen(3000, () => {
