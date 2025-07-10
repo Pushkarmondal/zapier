6b75df0541cd3fe15fbbc50ea09e9d7ac653683c
Pushkar Mondal <nishitm060@gmail.com>
2025-07-10 22:21:02 +0530

    Login signup routes and hased password ✅

--- primary_backend/index.ts
+++ primary_backend/index.ts
@@ -1,13 +1,23 @@
 import express from 'express';
 import { authMiddleware } from './middleware';
 import cors from 'cors';
-import { signupSchema } from './validation/types';
+import { loginSchema, signupSchema } from './validation/types';
+import  {PrismaClient} from '@prisma/client';
+import jwt from 'jsonwebtoken';
+import { JWT_SECRET } from './validation/config';
+import type { Request } from 'express';
+import bcrypt from "bcryptjs";
 
+const prisma = new PrismaClient();
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
 app.use(cors());
+
+interface AuthenticatedRequest extends Request {
+    id?: string;
+}
 
 app.post('/api/v1/auth/signup', async(req, res) => {
     try {
@@ -16,8 +26,24 @@ app.post('/api/v1/auth/signup', async(req, res) => {
         if (!parseData.success) {
             return res.status(400).json({ error: parseData.error.message })
         }
-        
-        
+
+        const getUser = await prisma.user.findFirst({
+            where: {
+                email: email
+            }
+        })
+        if (getUser) {
+            return res.status(400).json({ error: "User already exists" })
+        }
+        const registerUser = await prisma.user.create({
+            data: {
+                name,
+                email,
+                password: bcrypt.hashSync(password, 10)
+            }
+        })
+        return res.status(201).json(registerUser)
+
     } catch (error) {
         console.log(error);
         res.status(500).json({ error: "Internal Server Error" });
@@ -26,12 +52,57 @@ app.post('/api/v1/auth/signup', async(req, res) => {
 
 
 app.post('/api/v1/auth/login', async(req, res) => {
-    
+    try {
+        const {email, password} = req.body
+        const parseData = loginSchema.safeParse({email, password})
+        if (!parseData.success) {
+            return res.status(400).json({ error: parseData.error.message })
+        }
+        const getUser = await prisma.user.findFirst({
+            where: {
+                email: email
+            }
+        })
+        if (!getUser) {
+            return res.status(400).json({ error: "User not found" })
+        }
+        if (!bcrypt.compareSync(password, getUser.password)) {
+            return res.status(400).json({ error: "Incorrect password" })
+        }
+        //sign the jwt token ->
+        const token = jwt.sign({
+            id: getUser.id,
+        }, JWT_SECRET);
+        return res.status(200).json({getUser, token})
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
 })
 
 
-app.get('/api/v1/auth/getUser', authMiddleware, async(req, res) => {
-    
+app.get('/api/v1/auth/getUser', authMiddleware, async(req: AuthenticatedRequest, res) => {
+    try {
+        const id = req.id;
+            if (!id) {
+            return res.status(401).json({ error: "Unauthorized" });
+            }
+       const getUser = await prisma.user.findFirst({
+        where: {
+            id: Number(id)
+        }, select: {
+            name: true,
+            email: true
+        }
+       })
+       if (!getUser) {
+        return res.status(400).json({ error: "User not found" })
+       }
+       return res.status(200).json(getUser)
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
 })
 
 
--- primary_backend/middleware.ts
+++ primary_backend/middleware.ts
@@ -1,4 +1,22 @@
 import type { Request, Response, NextFunction } from "express";
+import { JWT_SECRET } from "./validation/config";
+import jwt from "jsonwebtoken";
 
-export const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
+interface AuthenticatedRequest extends Request {
+    id?: string;
+}
+
+export const authMiddleware = (req: AuthenticatedRequest, res: Response, next: NextFunction) => {
+    try {
+        const token = req.headers.authorization;
+        if (!token) {
+            return res.status(401).json({ error: "Unauthorized" });
+        }
+        const decoded = jwt.verify(token, JWT_SECRET) as { id: string };
+        req.id = decoded.id;
+        next();
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
 }
